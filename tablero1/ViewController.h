f8497f245052f4c70a2d1e0b78e7fc84af8c7aea
Paula <paulab20@hotmail.com>
2015-11-07 13:44:13 -0500

    Initial Commit

---
+++ tablero1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tablero1
+//
+//  Created by pau on 7/11/15.
+//  Copyright © 2015 pau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tablero1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tablero1
+//
+//  Created by pau on 7/11/15.
+//  Copyright © 2015 pau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
