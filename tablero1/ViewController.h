fa522102470988887fc27ee064404da6e47973b5
Paula <paulab20@hotmail.com>
2015-11-07 14:00:52 -0500

    validacion1

--- tablero1/ViewController.h
+++ tablero1/ViewController.h
@@ -13,3 +13,48 @@
 
 @end
 
+
+// Crear un tablero de velocidad
+//Implementar una numeracion
+
+enum velocidades:int {
+case apagado = 0
+case velocidadBaja = 20
+case velocidadMedia = 50
+case velocidadAlta = 120
+    
+    
+    init ( ){
+        
+        self = .apagado
+    }
+    
+}
+
+class auto{
+    
+    var velocidad = velocidades()
+    
+    init (velocidadInicial:velocidades){
+        self.velocidad = .apagado
+        
+    }
+    
+    
+    func cambioDeVelocidad(actual:Int, velocidadEnCadena : String) -> (Int, String){
+        
+        switch velocidad{
+        case.apagado : print(cambioDeVelocidad(0,velocidadEnCadena: "apagado"))
+        case.velocidadBaja : print(cambioDeVelocidad(20, velocidadEnCadena:"velocidadBaja"))
+        case.velocidadMedia : print (cambioDeVelocidad(50, velocidadEnCadena: "velocidadMedia"))
+        case.velocidadAlta : print (cambioDeVelocidad(120, velocidadEnCadena:"velocidadAlta"))
+            
+            
+            
+        }
+        
+        
+    }
+    
+    
+
